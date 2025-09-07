85916e4d9612271c0cdd38954dbdbe58c148fad5
Blatth <francoadrianmoran@gmail.com>
2025-09-07 20:18:48 -0300

    ejs tp

---
+++ TPs/TP1/TP1S/src/Histograma.hs
@@ -0,0 +1,78 @@
+-- | Un `Histograma` es una estructura de datos que permite contar cuántos valores hay en cada rango.
+-- @vacio n (a, b)@ devuelve un histograma vacío con n+2 casilleros:
+--
+-- * @(-inf, a)@
+-- * @[a, a + tamIntervalo)@
+-- * @[a + tamIntervalo, a + 2*tamIntervalo)@
+-- * ...
+-- * @[b - tamIntervalo, b)@
+-- * @[b, +inf)@
+--
+-- `vacio`, `agregar` e `histograma` se usan para construir un histograma.
+module Histograma
+  ( Histograma, -- No se exportan los constructores
+    vacio,
+    agregar,
+    histograma,
+    Casillero (..),
+    casMinimo,
+    casMaximo,
+    casCantidad,
+    casPorcentaje,
+    casilleros,
+  )
+where
+
+import Util
+
+data Histograma = Histograma Float Float [Int] -- i :: float = inicio, t :: float = tamaño, cs :: [Int] = contador de cuentas  
+  deriving (Show, Eq)
+
+-- | Inicializa un histograma vacío con @n@ casilleros para representar
+-- valores en el rango y 2 casilleros adicionales para los valores fuera del rango.
+-- Require que @l < u@ y @n >= 1@.
+vacio :: Int -> (Float, Float) -> Histograma
+vacio n (l, u) =
+  Histograma l size (replicate (n + 2) 0)
+  where
+    size = (u - l) / fromIntegral n         -- size lo calculé así porque sería calcular el intervalo [l,u) en n "subintervalos"
+
+
+-- | Agrega un valor al histograma.
+agregar :: Float -> Histograma -> Histograma
+agregar x (Histograma i t cs) = Histograma i t (actualizarElem idx (+1) cs)
+  where
+    n = length cs - 2
+    idx
+      | x < 1 = 0                             -- cae en el casillero 0 = (-inf, i)
+      | x >= i + fromIntegral n * t = n + 1   -- cae en el último casillero (n+1)
+      | otherwise = 1 + floor ((x - i) / t)   -- cae en alguno del medio, se calcula cual
+
+-- | Arma un histograma a partir de una lista de números reales con la cantidad de casilleros y rango indicados.
+histograma :: Int -> (Float, Float) -> [Float] -> Histograma
+histograma n r xs = error "COMPLETAR EJERCICIO 5"
+
+-- | Un `Casillero` representa un casillero del histograma con sus límites, cantidad y porcentaje.
+-- Invariante: Sea @Casillero m1 m2 c p@ entonces @m1 < m2@, @c >= 0@, @0 <= p <= 100@
+data Casillero = Casillero Float Float Int Float
+  deriving (Show, Eq)
+
+-- | Mínimo valor del casillero (el límite inferior puede ser @-inf@)
+casMinimo :: Casillero -> Float
+casMinimo (Casillero m _ _ _) = m
+
+-- | Máximo valor del casillero (el límite superior puede ser @+inf@)
+casMaximo :: Casillero -> Float
+casMaximo (Casillero _ m _ _) = m
+
+-- | Cantidad de valores en el casillero. Es un entero @>= 0@.
+casCantidad :: Casillero -> Int
+casCantidad (Casillero _ _ c _) = c
+
+-- | Porcentaje de valores en el casillero respecto al total de valores en el histograma. Va de 0 a 100.
+casPorcentaje :: Casillero -> Float
+casPorcentaje (Casillero _ _ _ p) = p
+
+-- | Dado un histograma, devuelve la lista de casilleros con sus límites, cantidad y porcentaje.
+casilleros :: Histograma -> [Casillero]
+casilleros _ = error "COMPLETAR EJERCICIO 6"
---
+++ TPs/TP1/TP1S/src/Util.hs
@@ -0,0 +1,39 @@
+module Util where
+
+-- | @alinearDerecha n s@ agrega espacios a la izquierda de @s@ hasta que su longitud sea @n@.
+-- Si @s@ ya tiene longitud @>= n@, devuelve @s@.
+alinearDerecha :: Int -> String -> String
+alinearDerecha n s = replicate (max 0 (n - length s)) ' ' ++ s
+
+{-
+Replicate x ' ' genera una cadena de x espacios, por lo que al hacer max 0 (n - length s) 
+me aseguro que replicate no tome un negativo, por lo que replica x veces ' ' y le concatena s.
+-}
+
+-- | Dado un índice y una función, actualiza el elemento en la posición del índice
+-- aplicando la función al valor actual. Si el índice está fuera de los límites
+-- de la lista, devuelve la lista sin cambios.
+-- El primer elemento de la lista es el índice 0.
+actualizarElem :: Int -> (a -> a) -> [a] -> [a]
+actualizarElem n f = zipWith (\i x -> if i == n then f x else x) [0 ..]
+
+
+{-
+Ej funcionamiento:
+actualizarElem 2 mult2 [4, 5, 6, 7, 8] = zipWith (\i x -> if i == 2 then mult2 x else x) [0 ..] [4, 5, 6, 7, 8]
+
+- zipWith agarra un elem de cada lista y los combina en pares: (0,4), (1,5), (2,6), (3,7), (4,8)
+- se verifica la condición \i x -> if i == 2 then mult2 x else x
+- (2,6): if 2 == 2 then mult2 6 else 6 => 12
+- devuelve como lista [4, 5, 12, 7, 8]
+-}
+
+-- https://learnyouahaskell.github.io/higher-order-functions.html#composition (ctrl+f zipwith)
+
+-- | infinito positivo (Haskell no tiene literal para +infinito)
+infinitoPositivo :: Float
+infinitoPositivo = 1 / 0
+
+-- | infinito negativo (Haskell no tiene literal para -infinito)
+infinitoNegativo :: Float
+infinitoNegativo = -(1 / 0)
